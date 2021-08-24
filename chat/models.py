4d0f82b9e8a86cc4f86ac763482d45cb2d8cc9b4
Filip Mularski <mularskif@gmail.com>
2021-08-24 13:56:02 +0200

    implemented login

--- chat/models.py
+++ chat/models.py
@@ -11,9 +11,11 @@ class User(AbstractUser):
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return f'{self.user.username}\'s profile'
+
 
 @receiver(post_save, sender=User)
 def user_pre_save_handler(instance, created, **kwargs):
     if created:
         UserProfile.objects.create(user=instance)
-        
--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from . import models
 
-# Create your views here.
+
+
+@login_required(login_url='login')
+def index(request):
+    context = {}
+    return render(request, 'chat/index.html', context)
+
+
+def login_user(request):
+    context = {}
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if not user:
+            messages.error(request, 'Invalid credentials.')
+            return redirect(reverse('login', ))
+
+        login(request, user)
+        return redirect(reverse('index', ))
+
+
+    return render(request, 'chat/login.html', context)
+
+
+def logout_user(request):
+    logout(request)
+    return redirect(reverse('login', ))
+
+
