6e2269721daf56e01f87b13dfad9134472cd1cfc
Filip Mularski <mularskif@gmail.com>
2021-08-25 21:14:37 +0200

    created custom reset password template

--- chat/urls.py
+++ chat/urls.py
@@ -9,7 +9,10 @@ urlpatterns = [
     path('logout/', views.logout_user, name='logout'),
     path('register/', views.register, name='register'),
 
-    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(
+        template_name='chat/reset_password_templates/reset_password.html'), 
+        name='password_reset'
+    ),
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     path('password_reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('password_reset/complete', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
